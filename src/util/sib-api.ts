e163ead68a1c0caa3af0adaa374caa3519c4a1c0
Alexey Savchenko <wasxela@gmail.com>
2023-05-23 20:08:56 +0300

    fix sib-mailer

--- src/util/sib-api.ts
+++ src/util/sib-api.ts
@@ -1,6 +1,7 @@
 import SibApi from "sib-api-v3-sdk";
+import * as dotenv from "dotenv";
 
-enum ENVELOPE {
+export enum ENVELOPE {
     "signUp",
     "confirmSignup",
     "changePass",
@@ -11,6 +12,8 @@ type emailContentType = {
     subject: string;
 };
 
+dotenv.config();
+
 SibApi.ApiClient.instance.authentications["api-key"].apiKey =
     process.env.SIB_API_KEY;
 
@@ -19,19 +22,19 @@ const apiInstance = new SibApi.TransactionalEmailsApi();
 export const sendEmail = async (
     emailAddresses: string | string[],
     emailType: ENVELOPE,
-    link?: string
+    token?: string
 ) => {
-    const message: emailContentType = getHtml(emailType);
-
+    const message: emailContentType = getHtml(emailType, token);
     apiInstance
         .sendTransacEmail({
             sender: { email: "shop@shop.my", name: "MyShop" },
-
-            messageVersions: {
-                to: emailAddressMap(emailAddresses),
-                htmlContent: message.htmlContent,
-                subject: message.subject,
-            },
+            subject: message.subject,
+            htmlContent: message.htmlContent,
+            messageVersions: [
+                {
+                    to: emailAddressMap(emailAddresses),
+                },
+            ],
         })
         .then(
             function (data) {
@@ -44,33 +47,38 @@ export const sendEmail = async (
 };
 
 const emailAddressMap = (emailAddresses: string | string[]) => {
-    const emailsMap = [...emailAddresses].map((email) => {
+    let emailsMap = [];
+    if (Array.isArray(emailAddresses)) {
+        emailsMap = [...emailAddresses];
+    } else {
+        emailsMap.push(emailAddresses);
+    }
+    return emailsMap.map((email) => {
         return { email };
     });
-    return emailsMap;
 };
 
-const getHtml = (emailType: ENVELOPE, link?: string): emailContentType => {
+const getHtml = (emailType: ENVELOPE, token?: string): emailContentType => {
     switch (emailType) {
         case ENVELOPE.signUp: {
-            return createSignUpHtml();
+            return createSignUpHtml(token);
         }
         case ENVELOPE.confirmSignup: {
             return createConfirmHtml();
         }
         case ENVELOPE.changePass: {
-            return createChangePassword(link);
+            return createChangePassword(token);
         }
         default:
             return;
     }
 };
 
-const createSignUpHtml = (): emailContentType => {
+const createSignUpHtml = (token: string): emailContentType => {
+    console.log("token => ", token);
     return {
-        htmlContent:
-            "<!DOCTYPE html><html><body><h1>Hello!</h1><p>May be you try signup to my SHOP!</p><div>For complate this action pleace click <a href='http://localhost:3000/login'> link </a></div></body></html>",
-        subject: "My subject, asshole!",
+        htmlContent: `<!DOCTYPE html><html><body><h1>Hello!</h1><p>May be you try signup to my SHOP!</p><div>For complate this action pleace click <a href="http://localhost:3000/login:${token}"> link </a></div></body></html>`,
+        subject: "Sign up to mySHOP",
     };
 };
 
@@ -78,98 +86,14 @@ const createConfirmHtml = (): emailContentType => {
     return {
         htmlContent:
             "<!DOCTYPE html><html><body><h1>Congratulations!</h1><p>You signup to my SHOP!</p></body></html>",
-        subject: "My subject, asshole!",
+        subject: "Confirm sign up",
     };
 };
 
-const createChangePassword = (link: string): emailContentType => {
+const createChangePassword = (token: string): emailContentType => {
     return {
         htmlContent:
             "<!DOCTYPE html><html><body><h1>Hello!</h1><p>Sombody try change your password!</p><p>For confirm click this <a href='http://localhost:3000'>link</a></body></html>",
         subject: "My subject, asshole!",
     };
 };
-
-// # Include the Brevo library\
-// var SibApiV3Sdk = require('sib-api-v3-sdk');
-// var defaultClient = SibApiV3Sdk.ApiClient.instance;
-// # Instantiate the client\
-// var apiKey = defaultClient.authentications['api-key'];
-// apiKey.apiKey = 'YOUR_API_V3_KEY';
-
-// var apiInstance = new SibApiV3Sdk.EmailCampaignsApi();
-// var emailCampaigns = new SibApiV3Sdk.CreateEmailCampaign();
-// # Define the campaign settings\
-// emailCampaigns.name = "Campaign sent via the API";
-// emailCampaigns.subject = "My subject";
-// emailCampaigns.sender = {"name": "From name", "email":"myfromemail@mycompany.com"};
-// emailCampaigns.type = "classic";
-// # Content that will be sent\
-// htmlContent: 'Congratulations! You successfully sent this example campaign via the Brevo API.',
-// # Select the recipients\
-// recipients: {listIds: [2, 7]},
-// # Schedule the sending in one hour\
-// scheduledAt: '2018-01-01 00:00:01'
-// }
-// # Make the call to the client\
-// apiInstance.createEmailCampaign(emailCampaigns).then(function(data) {
-// console.log('API called successfully. Returned data: ' + data);
-// }, function(error) {
-// console.error(error);
-// });
-
-// new SibApiV3Sdk.TransactionalEmailsApi()
-//     .sendTransacEmail({
-//         sender: { email: "sendinblue@sendinblue.com", name: "Sendinblue" },
-//         subject: "This is my default subject line",
-//         htmlContent:
-//             "<!DOCTYPE html><html><body><h1>My First Heading</h1><p>My first paragraph.</p></body></html>",
-//         params: {
-//             greeting: "This is the default greeting",
-//             headline: "This is the default headline",
-//         },
-//         messageVersions: [
-//             //Definition for Message Version 1
-//             {
-//                 to: [
-//                     {
-//                         email: "bob@example.com",
-//                         name: "Bob Anderson",
-//                     },
-//                     {
-//                         email: "anne@example.com",
-//                         name: "Anne Smith",
-//                     },
-//                 ],
-//                 htmlContent:
-//                     "<!DOCTYPE html><html><body><h1>Modified header!</h1><p>This is still a paragraph</p></body></html>",
-//                 subject: "We are happy to be working with you",
-//             },
-
-//             // Definition for Message Version 2
-//             {
-//                 to: [
-//                     {
-//                         email: "jim@example.com",
-//                         name: "Jim Stevens",
-//                     },
-//                     {
-//                         email: "mark@example.com",
-//                         name: "Mark Payton",
-//                     },
-//                     {
-//                         email: "andrea@example.com",
-//                         name: "Andrea Wallace",
-//                     },
-//                 ],
-//             },
-//         ],
-//     })
-//     .then(
-//         function (data) {
-//             console.log(data);
-//         },
-//         function (error) {
-//             console.error(error);
-//         }
-//     );
